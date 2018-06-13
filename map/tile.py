dd256fd86fe4a90ca52e5569842ed5f908801018
ComedicChimera <forlornsisu@gmail.com>
2018-06-12 21:11:24 -0400

    fixed tile class

--- map/tile.py
+++ map/tile.py
@@ -1,5 +1,6 @@
 class Tile:
-    def __init__(self, x, y, repeat_x=1, repeat_y=1, collidable=True):
-        self.x, self.y = x, y
+    def __init__(self, position, sprite_x, sprite_y, repeat_x=1, repeat_y=1, collidable=True):
+        self.position = position
+        self.sprite_x, self.sprite_y = sprite_x, sprite_y
         self.repeat_x, self.repeat_y = repeat_x, repeat_y
         self.collidable = collidable
